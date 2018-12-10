4e658adafc3be4cbb272c1dd6f6f2a6cc7de2d0c
notRiggle <aljaz.robokill@gmail.com>
2018-12-10 19:43:40 +0100

    Project setup

---
+++ MoreJPEGForms/MoreJPEG_UI/MainForm.cs
@@ -0,0 +1,24 @@
+﻿using NeedsMoreJPEG_core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoreJPEG_UI
+{
+    public partial class MainForm : Form
+    {
+        private readonly IMoreJPEG MoreJPEG;
+
+        public MainForm(IMoreJPEG moreJPEG)
+        {
+            MoreJPEG = moreJPEG;
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# NeedsMoreJPEG
+This project aims to bring you the lovely JPEG quality you always wished for right to your screen. It will probably never be finished. Woo.
