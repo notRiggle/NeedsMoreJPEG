68f9baff03fd12de5c3238645f74df1535aa46dd
notRiggle <aljaz.robokill@gmail.com>
2018-12-11 01:37:00 +0100

    Cleared unused imorts

--- MoreJPEGForms/MoreJPEG_UI/Program.cs
+++ MoreJPEGForms/MoreJPEG_UI/Program.cs
@@ -2,9 +2,6 @@
 using NeedsMoreJPEG_core;
 using NLog;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MoreJPEG_UI
--- MoreJPEGForms/NeedsMoreJPEG core/IMoreJPEG.cs
+++ MoreJPEGForms/NeedsMoreJPEG core/IMoreJPEG.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NeedsMoreJPEG_core
+﻿namespace NeedsMoreJPEG_core
 {
     public interface IMoreJPEG
     {
         string Test();
         void CrashTest();
+        void JPEGOnce();
     }
 }
